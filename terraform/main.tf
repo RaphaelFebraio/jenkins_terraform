546739dbf7339b5ee1c684742ca44f12db571d0b
Raphael Febraio Neto <106358542+RaphaelFebraio@users.noreply.github.com>
2023-01-29 12:31:24 -0300

    only commit

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a new Web Droplet in the nyc3 region
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins"
+  region   = var.region
+  size     = "s-2vcpu-2gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "k8s"
+  region  = var.region
+  version = "1.24.8-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+
+  }
+}
+
+
+variable "do_token" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
+
+//Mostrar o IP após o apply
+output "jenkins_ip" {
+  value =  digitalocean_droplet.jenkins.ipv4_address
+}
+
+//Criar o arquivo config na pasta do terraform
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
